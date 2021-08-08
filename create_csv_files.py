a359c7f770098d42fafd632264b466d64a83a5c0
Mojtaba Ahmadi <mogtabaahmadi9@gmail.com>
2021-08-08 14:11:20 +0430

    add create csv file module

---
+++ create_csv_files.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+import csv
+from constants import NUMBER_OF_INITIAL_FILES, NUMBER_OF_NEW_FILES, CSV_HEADER, CSV_DATA
+
+
+def create_csv_files(state):
+    """
+    Create csv sample files.
+
+    :param state: Specify where you are at the stage of creating the file: "initial" or "new".
+    :return: create csv sample files in "files" directory.
+    """
+
+    if state == 'initial':
+        for i in range(NUMBER_OF_INITIAL_FILES):
+            filename = str(datetime.now().date()) + '-' + str(i + 1)
+            write_csv_file(filename)
+
+    else:
+        for i in range(NUMBER_OF_NEW_FILES):
+            filename = str(datetime.now().date()) + '-' + str(NUMBER_OF_INITIAL_FILES + i + 1)
+            write_csv_file(filename)
+
+
+def write_csv_file(filename):
+    """
+    Write csv file function.
+
+    :param filename: csv file name.
+    :return: create csv sample files in "files" directory.
+    """
+
+    with open(f'files/{filename}.csv', 'w', encoding='UTF-8', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(CSV_HEADER)
+        writer.writerows(CSV_DATA)
+    print('CSV files created successfully')
