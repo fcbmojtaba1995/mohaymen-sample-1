175034f3cab252466d146983ccd071c43d06c22c
Mojtaba Ahmadi <mogtabaahmadi9@gmail.com>
2021-08-08 14:10:21 +0430

    add constants file

---
+++ constants.py
@@ -0,0 +1,12 @@
+# For create csv files
+NUMBER_OF_INITIAL_FILES = 100
+NUMBER_OF_NEW_FILES = 10
+
+CSV_HEADER = ['name', 'area', 'country_code2', 'country_code3']
+CSV_DATA = [
+    ['Albania', 28748, 'AL', 'ALB'],
+    ['Algeria', 2381741, 'DZ', 'DZA'],
+    ['American Samoa', 199, 'AS', 'ASM'],
+    ['Andorra', 468, 'AD', 'AND'],
+    ['Angola', 1246700, 'AO', 'AGO']
+]
