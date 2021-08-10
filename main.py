d3e672a54027de33da2941368c68257de8a74b69
Mojtaba Ahmadi <mogtabaahmadi9@gmail.com>
2021-08-11 03:29:08 +0430

    create main module updated.

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
+import time
+from create_csv_files import create_csv_files
+from redis_client import RedisClient
+from utils import create_files_metadata
+from constants import NUMBER_OF_INITIAL_FILES, NUMBER_OF_NEW_FILES
+
+
+def move_files_into_redis():
+    redis = RedisClient()
+    files_metadata = create_files_metadata()
+    if len(files_metadata) > 0:
+        redis.create_redis_pipeline('files', files_metadata)
+
+
+def start():
+    create_csv_files(NUMBER_OF_INITIAL_FILES)  # create initial csv files
+    move_files_into_redis()
+
+    time.sleep(10)
+
+    create_csv_files(NUMBER_OF_NEW_FILES)  # create new csv files
+    move_files_into_redis()
+
+
 if __name__ == '__main__':
-    pass
+    start()
